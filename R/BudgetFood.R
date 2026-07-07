507888544eb944be17fc310a1d940477f22c50aa
Darlan-Andrade <adarlandarocha@gmail.com>
2026-07-07 08:42:21 -0400

    Adicionando banco de dados BudgetFood ao pacote

---
+++ R/BudgetFood.R
@@ -0,0 +1,17 @@
+#' Banco de Dados: Despesas com Alimentação
+#'
+#' Um conjunto de dados contendo informações sobre a proporção de gastos
+#' com alimentação, despesas totais e características demográficas.
+#'
+#' @format Um data frame com 23.972 linhas e 7 colunas:
+#' \describe{
+#'   \item{rownames}{Identificador numérico da linha}
+#'   \item{wfood}{Proporção do orçamento gasto com alimentação}
+#'   \item{totexp}{Despesa total da família/indivíduo}
+#'   \item{age}{Idade da pessoa de referência}
+#'   \item{size}{Tamanho da família}
+#'   \item{town}{Tamanho da cidade/município (código)}
+#'   \item{sex}{Sexo da pessoa de referência (man/woman)}
+#' }
+#' @source Arquivo original BudgetFood.csv.
+"BudgetFood"
